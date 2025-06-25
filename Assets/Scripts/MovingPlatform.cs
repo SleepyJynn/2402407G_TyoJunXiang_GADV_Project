410ba2b821474b6f4a8b90fe910dd75ecfefcfbe
SleepyJynn <tjxrrokke@gmail.com>
2025-06-25 21:10:55 +0800

    finished a script for moving platforms
    had to search up on collision enter and exit so that i can parent the platform and player so when platform moves down player moves uniformly with it

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform posA;
+    public Transform posB;
+    bool moveDown = false;
+    bool moveUp = true;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForMove();
+        Debug.Log(moveUp);
+        Debug.Log(moveDown);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Set the current object as Parent of “Player”
+            collision.gameObject.transform.SetParent(this.gameObject.transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Unparent “Player” from the current object
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+    void CheckForMove()
+    {
+        if (moveUp == true)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, posB.position, Time.deltaTime * 2);
+            if (this.transform.position == posB.position)
+            {
+                moveUp = false;
+                moveDown = true;
+            }
+        }
+        if (moveDown == true)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, posA.position, Time.deltaTime * 2);
+            if (this.transform.position == posA.position)
+            {
+                moveUp = true;
+                moveDown = false;
+            }
+        }
+    }
+}
