abd8a0a2a385f5578cb321ff45a433c81715baa8
SleepyJynn <tjxrrokke@gmail.com>
2025-07-25 22:38:08 +0800

    added a flashing color for decayplatforms

--- Assets/Scripts/DecayPlatform.cs
+++ Assets/Scripts/DecayPlatform.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class DecayPlatform : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //flash color value
+    public Color FlashColor = Color.red;
+    public float FlashDuration = 0.1f;
+    //x amt of flashes before decay
+    public int FlashCount = 5;
+
     private bool triggered = false;
+    private SpriteRenderer sr;
+    //stores platform original color for switching
+    private Color originalColor;
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
     }
 
     // Update is called once per frame
@@ -27,7 +38,21 @@ public class DecayPlatform : MonoBehaviour
 
     IEnumerator Decay()
     {
-        yield return new WaitForSeconds(5f);
+        //total time platform will flash
+        float flashTime = FlashCount * FlashDuration * 2;
+        float waitTime = 5f - flashTime;
+
+        // Wait before starting flash (so platform lasts ~5s total)
+        yield return new WaitForSeconds(waitTime);
+
+        for (int i = 0; i < FlashCount; i++)
+        {
+            sr.color = FlashColor;
+            yield return new WaitForSeconds(FlashDuration);
+            sr.color = originalColor;
+            yield return new WaitForSeconds(FlashDuration);
+        }
+
         Destroy(gameObject);
     }
 }
