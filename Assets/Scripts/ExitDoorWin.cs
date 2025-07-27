35786070f595dd1513ab92f3e2b52d4dffa08214
SleepyJynn <tjxrrokke@gmail.com>
2025-07-27 13:43:18 +0800

    MainMenu UI

--- Assets/Scripts/ExitDoorWin.cs
+++ Assets/Scripts/ExitDoorWin.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinUIController : MonoBehaviour
 {
-    public TextMeshProUGUI winText;
     // Start is called before the first frame update
+    public TextMeshProUGUI winText;
+
+    private bool hasWon = false;
     void Start()
     {
         
@@ -15,7 +18,7 @@ public class WinUIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForRestart();
     }
 
     //when something enters the collider of the door
@@ -27,6 +30,7 @@ public class WinUIController : MonoBehaviour
             //set the winText to be active
             winText.gameObject.SetActive(true);
             Debug.Log("Game Won");
+            hasWon = true; // track win state
             //start the coroutine to have the winText fade in
             StartCoroutine(FadeInWin());
         }
@@ -46,6 +50,15 @@ public class WinUIController : MonoBehaviour
             winTextColorValues.a += Time.deltaTime / 2.5f;
             winText.color = winTextColorValues;
             yield return null;
+        }
+        Time.timeScale = 0.0f;
+    }
+    void CheckForRestart()
+    {
+        if (hasWon == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1.0f;
         }
     }
 }
